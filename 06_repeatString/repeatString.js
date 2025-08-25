ed46162efee1e07d1e62535a288d96c887fef41d
Slacker2519 <tienduongeng2001@gmail.com>
2025-08-25 09:05:10 +0000

    complete 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, loopNumber) {
+    if (loopNumber < 0)
+        return "ERROR";
 
+    let newString = '';
+    for (let i = 0; i < loopNumber; i++)
+    {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
