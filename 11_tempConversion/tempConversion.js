d4dac28540325b81fdd90a342e091dc8f2dda357
Slacker2519 <tienduongeng2001@gmail.com>
2025-08-26 10:25:46 +0000

    complete 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = ((fahrenheit - 32) * (5/9));
+  if (!Number.isInteger(celsius))
+    return parseFloat(celsius.toFixed(1));
+  else
+    return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * (9/5) + 32);
+  if (!Number.isInteger(fahrenheit))
+    return parseFloat(fahrenheit.toFixed(1));
+  else
+    return fahrenheit;
 };
 
 // Do not edit below this line
