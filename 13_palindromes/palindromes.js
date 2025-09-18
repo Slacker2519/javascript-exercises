3863ae534e2318fe1bf9d2dd50224e5ba834718d
Slacker2519 <tienduongeng2001@gmail.com>
2025-09-18 07:47:14 +0000

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let reverseStr = str.split('').reverse().join('');
 
+    return str == reverseStr;
 };
 
 // Do not edit below this line
