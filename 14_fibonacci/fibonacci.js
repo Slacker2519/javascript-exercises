8791f1457b5d0f8a778b8c2140127b89e7714d01
Slacker2519 <tienduongeng2001@gmail.com>
2025-09-18 08:14:12 +0000

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fibonacci = [0, 1];
+    for (let i = 2; i <= 100; i++) {
+        fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    }
 
+    if (Number.isInteger(Number(index))) {
+        if (Number(index) < 0) {
+            return "OOPS";
+        }
+        return fibonacci[index];
+    }
+
+    return "OOPS";
 };
 
 // Do not edit below this line
